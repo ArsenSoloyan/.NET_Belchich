c279d4bc0e6d7a6de389ca4cf173e78e0a1a8e4b
ArsenSoloyan <93132162+ArsenSoloyan@users.noreply.github.com>
2023-01-26 23:03:56 +0300

    Add files via upload

---
+++ Soloyan_PZ_3/Soloyan_PZ_3/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Soloyan_PZ_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(65, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Решить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(212, 202);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(62, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введите значение X:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(62, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Введите значение Y:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(187, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(187, 60);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(65, 95);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(222, 85);
+            this.textBox3.TabIndex = 6;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.Location = new System.Drawing.Point(312, 27);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(51, 19);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "sh(x)";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.Location = new System.Drawing.Point(312, 51);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(42, 19);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "x^2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton3.Location = new System.Drawing.Point(312, 74);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(42, 19);
+            this.radioButton3.TabIndex = 9;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "e^x";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ Soloyan_PZ_3/Soloyan_PZ_3/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Soloyan_PZ_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Visible = false;
+            button2.Visible = false;
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            textBox1.Visible = false;
+            textBox2.Visible = false;
+            textBox3.ReadOnly = true;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            label2.Visible = true;
+            textBox1.Visible = true;
+            textBox2.Visible = true;
+            textBox3.Visible = true;
+            button1.Visible = true;
+            button2.Visible = true;
+        }
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            label2.Visible = true;
+            textBox1.Visible = true;
+            textBox2.Visible = true;
+            textBox3.Visible = true;
+            button1.Visible = true;
+            button2.Visible = true;
+        }
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            label2.Visible = true;
+            textBox1.Visible = true;
+            textBox2.Visible = true;
+            textBox3.Visible = true;
+            button1.Visible = true;
+            button2.Visible = true;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+            button1.Visible = false;
+            button2.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            textBox1.Visible = false;
+            textBox2.Visible = false;
+            textBox1.Text = " ";
+            textBox2.Text = " ";
+            textBox3.Text = " ";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double y = Convert.ToDouble(textBox2.Text);
+            double x = Convert.ToDouble(textBox1.Text);
+            double F;
+            double i = 0;
+            if (radioButton1.Checked)
+            {
+                i = 1;
+            }
+            else if (radioButton2.Checked)
+            {
+                i = 2;
+            }
+            else if (radioButton3.Checked)
+            {
+                i = 3;
+            }
+            switch (i)
+            {
+                case 1:
+                    if (x - y == 0)
+                    {
+                        F = Math.Round(Math.Sin(Math.Sin(x)) + Math.Cos(Math.Sin(y)), 3);
+                        textBox3.Text += "X = " + x + Environment.NewLine + "Y = " + y + Environment.NewLine +
+                            "Разность 'x - y' равна нулю" + Environment.NewLine + "Ответ: " + F;
+                    }
+                    else if ( x - y > 0)
+                    {
+                        F = Math.Round(Math.Tan(Math.Sin(x + y)), 3);
+                        textBox3.Text += "X = " + x + Environment.NewLine + "Y = " + y + Environment.NewLine +
+                            "Разность 'x - y' больше нуля" + Environment.NewLine + "Ответ: " + F;
+                    }
+                    else if (x - y < 0)
+                    {
+                        F = Math.Round(Math.Pow(Math.Sin(Math.Sin(x)), 2) + Math.Pow(Math.Cos(Math.Sin(y)), 2), 3);
+                        textBox3.Text += "X = " + x + Environment.NewLine + "Y = " + y + Environment.NewLine +
+                            "Разность 'x - y' меньше нуля" + Environment.NewLine + "Ответ: " + F;
+                    }
+                    break;
+                case 2:
+                    if (x - y == 0)
+                    {
+                        F = Math.Round(Math.Sin(Math.Pow(x, 2)) + Math.Cos(Math.Pow(y, 2)), 3);
+                        textBox3.Text += "X = " + x + Environment.NewLine + "Y = " + y + Environment.NewLine +
+                            "Разность 'x - y' равна нулю" + Environment.NewLine + "Ответ: " + F;
+                    }
+                    else if (x - y > 0)
+                    {
+                        F = Math.Round(Math.Tan(Math.Pow(x, 2) + Math.Pow(y, 2)), 3);
+                        textBox3.Text += "X = " + x + Environment.NewLine + "Y = " + y + Environment.NewLine +
+                            "Разность 'x - y' больше нуля" + Environment.NewLine + "Ответ: " + F;
+                    }
+                    else if (x - y < 0)
+                    {
+                        F = Math.Round(Math.Pow(Math.Sin(Math.Pow(x, 2)), 2) + Math.Pow(Math.Cos(Math.Pow(y, 2)), 2), 3);
+                        textBox3.Text += "X = " + x + Environment.NewLine + "Y = " + y + Environment.NewLine +
+                            "Разность 'x - y' меньше нуля" + Environment.NewLine + "Ответ: " + F;
+                    }
+                    break;
+                case 3:
+                    if (x - y == 0)
+                    {
+                        F = Math.Round(Math.Sin(Math.Pow(Math.E, x)) + Math.Cos(Math.Pow(Math.E, y)), 3);
+                        textBox3.Text += "X = " + x + Environment.NewLine + "Y = " + y + Environment.NewLine +
+                            "Разность 'x - y' равна нулю" + Environment.NewLine + "Ответ: " + F;
+                    }
+                    else if (x - y > 0)
+                    {
+                        F = Math.Round(Math.Tan(Math.Pow(Math.E, x) + Math.Pow(Math.E, y)), 3);
+                        textBox3.Text += "X = " + x + Environment.NewLine + "Y = " + y + Environment.NewLine +
+                            "Разность 'x - y' больше нуля" + Environment.NewLine + "Ответ: " + F;
+                    }
+                    else if (x - y < 0)
+                    {
+                        F = Math.Round(Math.Pow(Math.Sin(Math.Pow(Math.E, x)), 2) + Math.Pow(Math.Cos(Math.Pow(Math.E, y)), 2), 3);
+                        textBox3.Text += "X = " + x + Environment.NewLine + "Y = " + y + Environment.NewLine +
+                            "Разность 'x - y' меньше нуля" + Environment.NewLine + "Ответ: " + F;
+                    }
+                    break;
+            }
+        }
+    }
+}

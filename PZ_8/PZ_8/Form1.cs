9396eb572e7478cf8fe2621adc40a95d31963ea4
ArsenSoloyan <93132162+ArsenSoloyan@users.noreply.github.com>
2023-04-04 23:14:29 +0300

    Add files via upload

---
+++ PZ_8/PZ_8/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+namespace PZ_8
+{
+    public partial class Form1 : Form
+    {
+            public Form1() => InitializeComponent();
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double Xmin = double.Parse(textBox1.Text);
+            double Xmax = double.Parse(textBox2.Text);
+            double Step = double.Parse(textBox3.Text);
+            double b = double.Parse(textBox4.Text);
+            int count = (int)Math.Ceiling((Xmax - Xmin) / Step) + 1;
+            double[] x = new double[count];
+            double[] y = new double[count];
+            double[] y2 = new double[count];
+            for (int i = 0; i < count; i++)
+            {
+                x[i] = Xmin + Step * i;
+                y[i]= Math.Pow(Math.Log(Math.Sin(Math.Pow(x[i], 3) + 0.0025)), 3 / 2)
+                    + 0.8 * Math.Pow(10, -3);
+                y2[i] = Math.Pow(x[i], 2);
+            }
+            chart1.ChartAreas[0].AxisX.Minimum = Xmin;
+            chart1.ChartAreas[0].AxisX.Maximum = Xmax;
+            chart1.ChartAreas[0].AxisX.MajorGrid.Interval = Step;
+            chart1.Series[0].Points.DataBindXY(x, y);
+            chart1.Series[1].Points.DataBindXY(x, y2);
+        }
+    }
+}
---
+++ PZ_8/PZ_8/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("PZ_8")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PZ_8")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("ac4219ba-c530-4f71-a86a-6bc6a5ee459d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
